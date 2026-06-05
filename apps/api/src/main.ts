598334097705f85a0559a2dd78e4a31cb9d12718
Md Emran Hossain <69568149+md-emran-hossain@users.noreply.github.com>
2026-06-05 08:30:12 +0600

    fix: remove unnecessary ValidationPipe from api bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -8,13 +7,6 @@ import { PORT } from './infrastructure/config/config-keys';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
 
   await app.listen(config.getOrThrow<number>(PORT));
 }
